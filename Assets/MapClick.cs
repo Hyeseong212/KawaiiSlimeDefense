f61660b8e59d318d80fd5400a657dbfcf1a7b192
Hyeseong212 <86096770+Hyeseong212@users.noreply.github.com>
2022-08-02 17:19:46 +0900

    backup

---
+++ Assets/MapClick.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapClick : MonoBehaviour
+{
+    [SerializeField] GameObject[] mapSize;
+    void Start()
+    {
+    }
+    void MapClickMethod()
+    {
+    }
+}
