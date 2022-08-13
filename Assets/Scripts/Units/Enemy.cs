e004c3ca8bbac39c78f9d4cb7df2b4902c1e5eb4
Hyeseong212 <86096770+Hyeseong212@users.noreply.github.com>
2022-08-13 18:51:13 +0900

    2

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -22,7 +22,7 @@ public class Enemy : MonoBehaviour
 
     WaitForSeconds delay =new WaitForSeconds(0.9f);
 
-    public EnemyData thisEnemydata;
+    public EnemyData thisEnemydata1;
     private void OnMouseEnter()
     {
 
